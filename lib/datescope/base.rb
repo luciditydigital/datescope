2bfc0279b96b934c971a802dc2e2006c4dca8a83
Nucc <nucc@lucidity.ie>
2012-10-25 10:46:05 +0100

    Fixes typo

--- lib/datescope/base.rb
+++ lib/datescope/base.rb
@@ -14,11 +14,11 @@ module DateScope
     end
 
     def last_month
-      where("created_at >= ? and created_ay <= ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month)
+      where("created_at >= ? and created_at <= ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month)
     end
 
     def month_before_last_month
-      where("created_at >= ? and created_ay <= ?", 2.month.ago.beginning_of_month, 2.month.ago.end_of_month)
+      where("created_at >= ? and created_at <= ?", 2.month.ago.beginning_of_month, 2.month.ago.end_of_month)
     end
 
     def this_year
